32fe4553197fc01ac7fc2bd3f39ddc644cd3ba61
Chris Williamson <cjwilliamson109@gmail.com>
2025-07-06 09:57:10 +0100

    Added a sprite sheet for asteroids and a cutting out system

--- entities/asteroid.py
+++ entities/asteroid.py
@@ -7,39 +7,19 @@ import math
 import pygame
 import random
 
-class Asteroid:
-    def __init__(self, x, y, velocity):
+class Asteroid(pygame.sprite.Sprite):
+    def __init__(self, image, x, y, velocity):
+        super().__init__()
+
+        self.original_image = image
+        self.image = image.copy()
+
         self.pos = pygame.Vector2(x, y)
-        self.size = random.randint(25, 75)
-        self.rect = pygame.Rect(x, y, self.size, self.size)
+        self.size = self.image.get_width() # asteroids are square
+        self.rect = pygame.Rect(x, y, self.image.get_width(), self.image.get_height())
         self.velocity = velocity
-        self.rotation_speed = 150
-    
-    # def rotate(self, direction, dt):
-    #     self.heading += direction * self.rotation_speed * dt
+        self.rotation_speed = 150 # TODO: rotate the asteroid
 
-    # def thrust(self, dt):
-    #     speed = 200
-    #     vector = angle_to_vector(self.heading, speed)
-    #     new_x = (self.pos.x + (vector.x * dt))
-    #     new_y = (self.pos.y + (vector.y * dt))
-
-    #     # Move the ship back into play if it's fully off screen
-    #     if self.pos.x > SCREEN_WIDTH and self.pos.x - SCREEN_WIDTH >= self.size:
-    #         new_x = 0
-    #     if self.pos.x <= -self.size:
-    #         new_x = SCREEN_WIDTH - self.size
-    #     if self.pos.y > SCREEN_HEIGHT and self.pos.y - SCREEN_HEIGHT >= self.size:
-    #         new_y = 0
-    #     if self.pos.y <= -self.size:
-    #         new_y = SCREEN_HEIGHT - self.size
-
-    #     self.pos.x = new_x
-    #     self.pos.y = new_y
-
-    #     # Sync rect to updated float position
-    #     self.rect.y = round(self.pos.y)
-    #     self.rect.x = round(self.pos.x)
     def move(self, dt):
         new_x = (self.pos.x + (self.velocity.x * dt))
         new_y = (self.pos.y + (self.velocity.y * dt))
@@ -59,6 +39,3 @@ class Asteroid:
         # Sync rect to updated float position
         self.rect.y = round(self.pos.y)
         self.rect.x = round(self.pos.x)
-
-    def draw(self, surface):
-        pygame.draw.circle(surface, 'white', (self.rect.x + (self.size // 2), self.rect.y + (self.size // 2)), self.size, 1)
--- state/play_state.py
+++ state/play_state.py
@@ -1,3 +1,6 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from enum import Enum
 from constants.constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from entities.ship import Ship
 from entities.asteroid import Asteroid
@@ -7,15 +10,56 @@ from gameutils.state.state import State
 import random
 import pygame
 
+@dataclass
+class Sprite:
+    name: str
+    pos: tuple[int, int]
+    size: tuple[int, int]
+
+class SpriteSheet:
+    def __init__(self, file, sprites):
+        self.sprite_sheet_image = pygame.image.load(file).convert_alpha()
+        self.sprites = sprites
+
+    def get_sprites(self):
+        sprites = {}
+        for sprite in self.sprites:
+            sprites[sprite.name] = self._get_sprite_surface(sprite)
+        return sprites
+
+    def _get_sprite_surface(self, sprite):
+        """Extracts a single sprite from the sheet image."""
+        sprite_surface = pygame.Surface(sprite.size, pygame.SRCALPHA)  # transparent background
+        sprite_surface.blit(self.sprite_sheet_image, (0, 0), pygame.Rect(sprite.pos, sprite.size))
+        return sprite_surface
+
+class AsteroidSpriteName(Enum):
+    LARGE_1 = 'large_1'
+    LARGE_2 = 'large_2'
+    LARGE_3 = 'large_3'
+
+class AsteroidsSpriteSheet(SpriteSheet):
+    def __init__(self):
+        file = 'assets/asteroids-2x.png'
+        sprites = [
+            Sprite(AsteroidSpriteName.LARGE_1, (0, 0), (160, 160)),
+            Sprite(AsteroidSpriteName.LARGE_2, (160, 0), (160, 160)),
+            Sprite(AsteroidSpriteName.LARGE_3, (320, 0), (160, 160)),
+        ]
+        super().__init__(file, sprites)
+
+
 class PlayState(State):
     def __init__(self, game_manager):
         super().__init__(game_manager)
 
         self.ship = Ship((SCREEN_WIDTH // 2) - 10, (SCREEN_HEIGHT // 2) - 10)
-        self.asteroids = set()
-        self.asteroids.add(self.spawn_asteroid())
         self.time_since_last_asteroid = 0
         self.asteroid_spawn_interval = 1
+        sprite_sheet = AsteroidsSpriteSheet()
+        self.sprite_surfaces = sprite_sheet.get_sprites()
+        self.asteroids = pygame.sprite.Group()
+        self.asteroids.add(self.spawn_asteroid())
 
     def spawn_asteroid(self):
         edge = random.choice(['top', 'bottom', 'left', 'right'])
@@ -40,7 +84,8 @@ class PlayState(State):
         speed = random.uniform(100, 300)
         velocity = direction * speed
         print(x, y)
-        return Asteroid(x, y, velocity)
+        choice = random.choice([AsteroidSpriteName.LARGE_1, AsteroidSpriteName.LARGE_2, AsteroidSpriteName.LARGE_3])
+        return Asteroid(self.sprite_surfaces[choice], x, y, velocity)
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
@@ -63,16 +108,21 @@ class PlayState(State):
             self.time_since_last_asteroid = 0
 
         # Check for asteroid collisions
-        new_asteroids = set()
-        for asteroid in self.asteroids:
-            if self.ship.rect.colliderect(asteroid.rect):
-                continue
-            new_asteroids.add(asteroid)
-        self.asteroids = new_asteroids
+        # new_asteroids = set()
+        # for asteroid in self.asteroids:
+        #     if self.ship.rect.colliderect(asteroid.rect):
+        #         continue
+        #     new_asteroids.add(asteroid)
+        # self.asteroids = new_asteroids
 
     def render(self):
         self.screen.fill('black')
 
         self.ship.draw(self.screen)
-        for asteroid in self.asteroids:
-            asteroid.draw(self.screen)
+        # for asteroid in self.asteroids:
+        #     asteroid.draw(self.screen)
+
+        self.asteroids.draw(self.screen)
+
+        for sprite in self.asteroids.sprites():
+            pygame.draw.rect(self.screen, 'red', sprite.rect, 1)
